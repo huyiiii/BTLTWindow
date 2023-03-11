23e3b8362ecbdf79c263c037bb3f2836487ed5bb
soncon2052003 <90188775+soncon2052003@users.noreply.github.com>
2023-03-11 08:41:45 +0700

    ban san pham test

--- WindowsFormsAppNhom/banSp.Designer.cs
+++ WindowsFormsAppNhom/banSp.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ma_don_hang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.thong_tin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ten_don_hang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Duyệt vận chuyển";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(483, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Hủy đơn";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ma_don_hang,
+            this.thong_tin,
+            this.ten_don_hang});
+            this.dataGridView1.Location = new System.Drawing.Point(2, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(797, 359);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // ma_don_hang
+            // 
+            this.ma_don_hang.HeaderText = "Mã đơn hàng";
+            this.ma_don_hang.Name = "ma_don_hang";
+            // 
+            // thong_tin
+            // 
+            this.thong_tin.HeaderText = "Thông tin đơn hàng";
+            this.thong_tin.Name = "thong_tin";
+            // 
+            // ten_don_hang
+            // 
+            this.ten_don_hang.HeaderText = "Tên đơn hàng";
+            this.ten_don_hang.Name = "ten_don_hang";
+            // 
+            // banSp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "banSp";
             this.Text = "banSp";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ma_don_hang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn thong_tin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ten_don_hang;
     }
 }
