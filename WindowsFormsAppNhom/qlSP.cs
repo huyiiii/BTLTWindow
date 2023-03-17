57f7b37247850529af3e5dbe95fb033692b9942f
sonhv <90188775+soncon2052003@users.noreply.github.com>
2023-03-17 08:32:31 +0700

    them quan ly sp, ban sp

--- WindowsFormsAppNhom/qlSP.cs
+++ WindowsFormsAppNhom/qlSP.cs
@@ -8,13 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsAppNhom
+namespace sanpham
 {
-    public partial class qlSP : Form
+    public partial class Form1 : Form
     {
-        public qlSP()
+        public Form1()
         {
             InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
